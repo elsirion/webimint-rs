286e1ad516d9d9e6ceadc556c99f7039ec1ff4c7
Jens Krause <email@jkrause.io>
2023-08-14 19:51:37 +0200

    Support Tailwind

--- src/components/app.rs
+++ src/components/app.rs
@@ -1,10 +1,9 @@
-use crate::components::Joined;
-use leptos::ev::SubmitEvent;
+use crate::components::{Joined, Logo};
 
 use crate::client::ClientRpc;
 use crate::context::provide_client_context;
 use crate::utils::empty_view;
-use leptos::html::Input;
+use leptos::html::Textarea;
 use leptos::*;
 
 //
@@ -21,65 +20,83 @@ pub fn App(cx: Scope) -> impl IntoView {
         }
     });
 
-    let invite_code_element: NodeRef<Input> = create_node_ref(cx);
+    let invite_code_element: NodeRef<Textarea> = create_node_ref(cx);
 
-    let on_submit_join = move |ev: SubmitEvent| {
-        ev.prevent_default();
+    // TODO: Validate invite code (create_effect ...)
+    let (invite_code, set_invite_code) = create_signal::<String>(cx, "".to_string());
 
-        let invite = invite_code_element.get().expect("<input> to exist").value();
+    let on_submit_join = move || {
+        let invite = invite_code.get();
         join_action.dispatch(invite);
     };
 
     let joined = move || join_action.value().get().is_some();
+    let disabled = move || invite_code.get().chars().count() == 0;
 
     view! { cx,
+      <div class="h-[100dvh] w-full max-w-[600px] mx-auto p-6">
 
-      <Show
-      when=move || !joined()
-        fallback=|_| empty_view()
-        >
-        <p>"Join a federation"</p>
-        <form on:submit=on_submit_join>
-            // TODO: Validate invite code. Listen to `on:change`
-            <input
-                type="text"
+        <header class="w-full mb-20">
+          <Logo />
+        </header>
+        <main class="w-full flex flex-col">
+          <Show
+            when=move || !joined()
+              fallback=|_| empty_view()
+            >
+            <h1 class="font-heading text-gray-900 text-4xl font-semibold">"Join a Federation"</h1>
+            <p class="font-body text-gray-900 text-xl">"Enter invite code (i.e. fed11jpr3lgm8t…) to join a Federation"</p>
+            <form
+              class="flex flex-col gap-2"
+              on:submit=move |ev| {
+                ev.prevent_default();
+                on_submit_join();
+              }
+              >
+              <textarea
+                class="my-8 w-full text-xl font-body"
+                rows="4"
                 node_ref=invite_code_element
-                placeholder="Invite Code, i.e. fed11jpr3lgm8t…"
-                prop:disabled=joined()
-            />
-            <input
+                placeholder="Invite Code"
+                on:input=move |ev| {
+                  set_invite_code.set(event_target_value(&ev));
+                }
+              />
+              <input
                 type="submit"
-                value="Join Federation"
-                prop:disabled=joined()
-            />
-        </form>
+                value="Join"
+                class="fm-btn-primary w-fit text-2xl"
+                disabled=move || disabled()
+              />
+            </form>
 
-      </Show>
+          </Show>
 
 
-      <Show when=move || join_action.pending().get()
-        fallback=|_| empty_view()
-        >
-        <p>"Joining ..."</p>
-      </Show>
+          <Show when=move || join_action.pending().get()
+            fallback=|_| empty_view()
+            >
+            <p>"Joining ..."</p>
+          </Show>
 
-      <Suspense
-        fallback=move || view!{ cx, "Loading..."}
-      >
-      <ErrorBoundary fallback=|cx, error| view!{ cx, <p>{format!("Failed to create client: {:?}", error.get())}</p>}>
-      { move || {
-        join_action.value().get().map(|c| {
-          // Create app context to provide ClientRpc
-          // as soon as it's available
-          provide_client_context(cx, c);
+          <Suspense
+            fallback=move || view!{ cx, "Loading..."}
+          >
+          <ErrorBoundary fallback=|cx, error| view!{ cx, <p>{format!("Failed to create client: {:?}", error.get())}</p>}>
+          { move || {
+            join_action.value().get().map(|c| {
+              // Create app context to provide ClientRpc
+              // as soon as it's available
+              provide_client_context(cx, c);
 
-          view! { cx,
-            <Joined />
-          }
-        })
-      }}
-      </ErrorBoundary>
-      </Suspense>
-
+              view! { cx,
+                <Joined />
+              }
+            })
+          }}
+          </ErrorBoundary>
+          </Suspense>
+        </main>
+      </div>
     }
 }
--- src/components/mod.rs
+++ src/components/mod.rs
@@ -1,11 +1,13 @@
 pub mod app;
 pub mod balance;
 pub mod joined;
+pub mod logo;
 pub mod receive;
 pub mod send;
 
 pub use app::*;
 pub use balance::*;
 pub use joined::*;
+pub use logo::*;
 pub use receive::*;
 pub use send::*;
