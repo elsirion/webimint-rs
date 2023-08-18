67a59b28738bd787998a52cf7839eb2a03e2e288
elsirion <elsirion@protonmail.com>
2023-08-18 22:25:50 +0200

    feat: allow scanning

--- src/components/submit_form.rs
+++ src/components/submit_form.rs
@@ -1,5 +1,6 @@
 use leptos::ev::KeyboardEvent;
 use leptos::*;
+use leptos_qr_scanner::Scan;
 
 use crate::components::SubmitButton;
 
@@ -18,42 +19,82 @@ where
     let (value, set_value) = create_signal(cx, String::new());
 
     let button_is_disabled = Signal::derive(cx, move || loading.get() || value.get().is_empty());
+    let scan_disabled = Signal::derive(cx, move || loading.get());
+
+    let (scan, set_scan) = create_signal(cx, false);
+
+    let textarea = view! {cx,
+        <textarea
+          class="my-8 w-full text-xl font-body text-gray-600 border-gray-400 placeholder:text-gray-400 ring-0 focus:border-blue-400 focus:ring-0"
+          rows="4"
+          required
+          placeholder=placeholder.clone()
+          prop:disabled=move || loading.get()
+          prop:value=move || value.get()
+          on:keydown=move |ev: KeyboardEvent| {
+            if ev.key() == "Enter" {
+              ev.prevent_default();
+              on_submit(value.get());
+            }
+          }
+          on:input=move |ev| {
+            let val = event_target_value(&ev);
+            set_value.set(val);
+          }
+          on:paste=move |ev| {
+            let val = event_target_value(&ev);
+            set_value.set(val);
+          }
+        />
+    };
+
+    let qr_scanner = view! { cx,
+        <Scan
+          active=scan
+          on_scan=move |invite| {
+            set_scan.set(false);
+            set_value.set(invite.clone());
+            on_submit(invite);
+          }
+          class="my-8"
+        />
+    };
 
     view! { cx,
       <form on:submit=|ev| ev.prevent_default()>
 
       <p class="font-body text-gray-600 text-xl">{description}</p>
-      <textarea
-        class="my-8 w-full text-xl font-body text-gray-600 border-gray-400 placeholder:text-gray-400 ring-0 focus:border-blue-400 focus:ring-0"
-        rows="4"
-        required
-        placeholder=placeholder
-        prop:disabled=move || loading.get()
-        prop:value=move || value.get()
-        on:keydown=move |ev: KeyboardEvent| {
-          if ev.key() == "Enter" {
-              ev.prevent_default();
-              on_submit(value.get());
-          }
-        }
-        on:input=move |ev| {
-          let val = event_target_value(&ev);
-          set_value.set(val);
-        }
-        on:paste=move |ev| {
-          let val = event_target_value(&ev);
-          set_value.set(val);
-        }
-      />
+      <Show
+        when=move || scan.get()
+        fallback=move |_| textarea.clone()
+      >
+        {qr_scanner.clone()}
+      </Show>
 
+      <div class="flex space-x-4">
       <SubmitButton
-          class="w-full"
+        class="w-5/6"
         loading=loading
         disabled=button_is_disabled
         on_click=move |_| {
           on_submit(value.get());
         }
       >{submit_label}</SubmitButton>
+      <SubmitButton
+        class="w-1/6"
+        loading=loading
+        disabled=scan_disabled
+        on_click=move |_| {
+          set_scan.set(!scan.get());
+        }
+      >{move || {
+        if scan.get() {
+          "Stop"
+        } else {
+          "Scan"
+        }
+      }}</SubmitButton>
+      </div>
       </form>
     }
 }
