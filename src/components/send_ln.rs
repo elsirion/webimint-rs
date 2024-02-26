08e86510490b109e6106f28e691320ddadcf485f
toyota-corolla0 <toyota_corolla0@proton.me>
2024-02-26 12:37:02 -0600

    feat: send ecash component also renamed a few components

--- src/components/send_ln.rs
+++ src/components/send_ln.rs
@@ -4,35 +4,36 @@ use crate::components::SubmitForm;
 use crate::context::ClientContext;
 
 //
-// Receive e-cash component
+// Send LN component
 //
 #[component]
-pub fn Receive() -> impl IntoView {
+pub fn SendLn() -> impl IntoView {
     let ClientContext { client, .. } = expect_context::<ClientContext>();
 
     let client = client.clone();
     let submit_action = create_action(move |invoice: &String| {
         let invoice = invoice.clone();
-        async move { client.get_value().receive(invoice).await }
+        async move { client.get_value().ln_send(invoice).await }
     });
 
     view! {
 
       <SubmitForm
-        description="Enter e-cash notes (i.e. BAQB6ijaAs0mXNoyKYvhI…) to redeem".into()
+        description="Enter LN invoice (i.e. lnbcrt1p0…) to send a payment".into()
         on_submit=move |v| submit_action.dispatch(v)
-        placeholder="e-cash notes".into()
-        submit_label="Redeem".into()
+        placeholder="LN invoice".into()
+        submit_label="Send".into()
         loading=submit_action.pending()
       />
+
 
       {move ||
         if let Some(result) = submit_action.value().get() {
           view!(
             <div class="text-body text-md mt-4">{
               match result {
-                Err(error) => view!(<span class="text-red-500">{format!("✗ Failed to redeem e-cash: {error}")}</span>),
-                Ok(value) => view!(<span class="text-green-600">{format!("✓ Redeemed {:?} msat", value.msats)}</span>)
+                Err(error) => view!(<span class="text-red-500">{format!("✗ Failed to send invoice {error}")}</span>),
+                Ok(_) => view!(<span class="text-green-600">"✓ Invoice successfully sent"</span>)
               }
             }
             </div>)
